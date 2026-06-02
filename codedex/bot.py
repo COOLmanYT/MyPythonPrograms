808f1364049c610cea02845fc3d9aea2ed6f3299
COOLmanYT <230516181+COOLmanYT@users.noreply.github.com>
2026-06-02 12:29:20 +1000

    added comments to bot.py

--- codedex/bot.py
+++ codedex/bot.py
@@ -10,24 +10,24 @@ if not discord_token: # Check if the token is missing.
   raise RuntimeError('DISCORD_TOKEN is missing. Check your .env file.')
 
 def get_meme():
-  response = requests.get('https://meme-api.com/gimme')
+  response = requests.get('https://meme-api.com/gimme') # get meme from API.
   json_data = json.loads(response.text)
-  return json_data['url']
+  return json_data['url'] # return meme URL.
 
 class MyClient(discord.Client):
   async def on_ready(self):
-    print('Logged on as {0}!'.format(self.user))
+    print('Logged on as {0}!'.format(self.user)) # print a confirmation that bot is logged in
 
-  async def on_message(self, message):
+  async def on_message(self, message): # when a message is sent:
     if message.author == self.user:
-      print('Ignoring self-message')
+      print('Ignoring self-message') # ignore messages sent by the bot
       return
-    if message.content.startswith('$meme'):
-      await message.channel.send(get_meme())
-      print('Meme sent!')
+    if message.content.startswith('$meme'): # if the message starts with $meme:
+      await message.channel.send(get_meme()) # send the meme URL.
+      print('Meme sent!') # print confirmation that meme was sent.
 
 intents = discord.Intents.default()
-intents.message_content = True
+intents.message_content = True # enable message content intent - check dev portal if it is enabled for the bot
 
 client = MyClient(intents=intents)
-client.run(discord_token) # Replace with your own token.
+client.run(discord_token) # run the bot
